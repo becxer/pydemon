e0985dbe4c8eff940fe274bd535be0e174ea3752
becxer <becxer87@gmail.com>
2016-01-29 20:58:13 +0900

    fix emacs backupfile missing ignoreing

--- pydemon.py
+++ pydemon.py
@@ -22,107 +22,114 @@ pydemon_dat = '.pydemon.dat'
 dat_obj = {}
 
 def set_bashCmd():
-	global bashCmd
-	bashCmd = ""
-	for i in range(len(sys.argv)):
-		if i > 0 :
-			if os.path.isfile(sys.argv[1]) and ('.sh' in sys.argv[1]):
-				f = open(sys.argv[1],"r")
-				bashCmd = f.read()
-				f.close()
-				break
-			bashCmd += sys.argv[i] + " "
+    global bashCmd
+    bashCmd = ""
+    for i in range(len(sys.argv)):
+        if i > 0 :
+            if os.path.isfile(sys.argv[1]) and ('.sh' in sys.argv[1]):
+                f = open(sys.argv[1],"r")
+                bashCmd = f.read()
+                f.close()
+                break
+            bashCmd += sys.argv[i] + " "
 
 def set_now_time(path):
-	global now_time
-	global dat_obj
-	dirs = os.listdir(path)
-	for f in dirs:
-		fnow = path+'/'+f
-		isIgnored = False
-		for postfix in dat_obj['ignore_postfix']:
-			if fnow.endswith(postfix) :
-				isIgnored = True
-				break
-		if os.path.isdir(fnow) and not isIgnored:
-			set_now_time(fnow)
-		elif os.path.getmtime(fnow) > now_time and not isIgnored:
-                        print fnow + " is changed"
-			now_time = os.path.getmtime(fnow)
+    global now_time
+    global dat_obj
+    dirs = os.listdir(path)
+    for f in dirs:
+        fnow = path+'/'+f
+        isIgnored = False
+        for postfix in dat_obj['ignore_postfix']:
+            if f.endswith(postfix) :
+                isIgnored = True
+                break
+        for prefix in dat_obj['ignore_prefix']:
+            if f.startswith(prefix) :
+                isIgnored = True
+                break
+        if not isIgnored:
+            if os.path.isdir(fnow):
+                set_now_time(fnow)
+            elif os.path.getmtime(fnow) > now_time:
+                print fnow + " is changed"
+                now_time = os.path.getmtime(fnow)
 
 def run_cmd(bashCmd):
-	print str(bashCmd)
-	print "[stdout]----------------------"
-	process = subprocess.Popen(bashCmd, shell=True 
-				,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	res = ''
-	while True:
-		output = process.stdout.readline()
-		if output == '' and process.poll() is not None:
-			break
-		if output:
-			res += output
-			print output.strip()
-		rc = process.poll()
-	output, error = process.communicate()
-	print "[stderr]----------------------"
-	res += error
-	print error
-	if process.returncode != 0 :
-		print "[ERROR] process error"
-	print "pydemon is watching your directory..."
-	return res
+    print str(bashCmd)
+    print "[stdout]----------------------"
+    process = subprocess.Popen(bashCmd, shell=True 
+                ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = ''
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            res += output
+            print output.strip()
+        rc = process.poll()
+    output, error = process.communicate()
+    print "[stderr]----------------------"
+    res += error
+    print error
+    if process.returncode != 0 :
+        print "[ERROR] process error"
+    print "pydemon is watching your directory..."
+    return res
 
 def load_dat(datpath):
-	if os.path.isfile(datpath):
-		datf = open(datpath,'r')
-		js = json.loads(datf.read())
-		datf.close()
-		return js
-	else:
-		return {'run_count':0 , \
-				'ignore_postfix':[\
-				".swp", ".log", ".git",".dat"]}
+    if os.path.isfile(datpath):
+        datf = open(datpath,'r')
+        js = json.loads(datf.read())
+        datf.close()
+        return js
+    else:
+        return {'run_count':0 , \
+                                'ignore_prefix' :[\
+                                ".","#","~"],
+                'ignore_postfix':[\
+                ".swp", ".log", ".git",".dat"]}
 
 def save_dat(obj ,datpath):
-	datf = open(datpath,'w')
-	json.dump(obj,datf,indent=4)
-	datf.close()
-	return None
+    datf = open(datpath,'w')
+    json.dump(obj,datf,indent=4)
+    datf.close()
+    return None
 
 def main_while():
-	global dat_obj
-	global now_time
-	global last_time
-	dat_obj = load_dat(pydemon_dat)
-	while True:
-		if last_time != now_time :
-			
-			set_bashCmd()
-			
-			dat_obj = load_dat(pydemon_dat)
-			dat_obj['run_count'] += 1
-			
-			print "[#"+str(dat_obj['run_count'])+"]---------------------------"
-			logf = open(pydemon_log,"a")
-			logf.write("T: " +str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-						+" #"+str(dat_obj['run_count'])+" Log\n")
-			logf.write(run_cmd(bashCmd))
-			logf.close()
-			
-			save_dat(dat_obj,pydemon_dat)
-			
-			set_now_time('.')
-			last_time = now_time
-		else :	
-			set_now_time('.')	
-		time.sleep(sleep_time)
+    global dat_obj
+    global now_time
+    global last_time
+    dat_obj = load_dat(pydemon_dat)
+    while True:
+        if last_time != now_time :
+            
+            set_bashCmd()
+            
+            dat_obj = load_dat(pydemon_dat)
+            dat_obj['run_count'] += 1
+            
+            print "[#"+str(dat_obj['run_count'])+"]---------------------------"
+            logf = open(pydemon_log,"a")
+            logf.write("T: " +str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        +" #"+str(dat_obj['run_count'])+" Log\n")
+            logf.write(run_cmd(bashCmd))
+            logf.close()
+            
+            save_dat(dat_obj,pydemon_dat)
+            
+            set_now_time('.')
+            last_time = now_time
+        else :    
+            set_now_time('.')    
+        time.sleep(sleep_time)
 
 def main():
-	if len(sys.argv) == 1:
-		print 'usage : ./pydemon "bash command" or your-script.sh'
-		exit()
-	main_while()
+    if len(sys.argv) == 1:
+        print 'usage : ./pydemon "bash command" or your-script.sh'
+        exit()
+    main_while()
 
 if __name__  == "__main__":
-	main()
+    main()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README') as file:
 
 setup(
 	name='pydemon',
-	version='0.0.12',
+	version='0.0.13',
 	py_modules=['pydemon'],
 	author ='becxer',
 	author_email='becxer87@gmail.com',
